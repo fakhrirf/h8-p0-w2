e01c3f59d8f187c7fc1dd3170a3d619606e9aea6
fakhrirf <fakhririyadh@gmail.com>
2020-03-24 16:54:49 +0700

    first commit

---
+++ 1.Senin/anchor.js
@@ -0,0 +1,54 @@
+ // 1. Newton Second Law 
+
+ /* 
+
+  Read mass of object with 600 kg.
+  Read object's acceleration with 2 ms/s.
+  Read resultant force (newton).
+
+  Compute mass of object times object's acceleration
+  
+  DISPLAY resultant force
+  
+*/
+
+//Tahun Kabisat  
+
+/* 
+
+Set year with any value.
+IF year divided by 4 equals to 0 and year divided by 100 not equals to 0
+    DISPLAY kabisat year
+IF ELSE year divided by 4 equals to 0 and year divided by 100 equals to 100 and year divided equals to 400
+    DISPLAY leap year
+    ELSE;
+        DISPLAY not leap year 
+        
+*/
+
+// Laundry Day 
+
+/* 
+
+Set shirt with 0 
+WHILE shirt <= 20
+      DISPLAY shirt
+      INCREMENT shirt 
+        (shirt += 1)
+      DO Washing Machine ON
+
+// Periksa Kuku
+
+/*
+
+Set student with 0
+
+WHILE student <= 40
+      INCREMENT student 
+        (student += 1)
+      IF student equals to long nails
+            DO punish the student
+      ELSE
+            DO praise the student
+
+*/
