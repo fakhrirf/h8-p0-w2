2f070202b23ff6f16da5b1e75dec11e78a0ab244
fakhrirf <fakhririyadh@gmail.com>
2020-03-29 18:30:33 +0700

    Update looping.js
    "change commit"

--- 3.Rabu/looping.js
+++ 3.Rabu/looping.js
@@ -13,7 +13,7 @@
 // }
 
 //2. looping dengan for 
-
+/*
 console.log('Looping Pertama')
 for (var loopFor = 1; loopFor <= 20; loopFor++){
     console.log(loopFor + ' - I Love coding');
@@ -24,5 +24,38 @@ for (var loopFor = 20; loopFor > 0; loopFor--){
     console.log(loopFor + ' - I will become fullstack developer');
 }
 
+for (var counterloop = 1; counterloop <= 100; counterloop++){
+    if(counterloop % 2 !== 0){
+        console.log("Ganjil")
+    }
+    else{
+        console.log("Genap")
+    } 
+}
 
+for (var counterloop = 1; counterloop < 100; counterloop +=2){
+    if(counterloop % 3 == 0){
+        console.log('3 Kelipatan 3')
+    }
+    else {
+        console.log('')
+    }
+}
 
+for (var counterloop = 1; counterloop < 100; counterloop +=5){
+    if (counterloop % 6 == 0){
+        console.log('6 kelipatan 6')
+    } else {
+        console.log('')
+    }
+}
+
+for (var counterloop = 1; counterloop < 100; counterloop +=9){
+    if (counterloop % 10 == 0){
+        console.log(' 10 kelipatan 10')
+    }
+    else{
+        console.log('')
+    } 
+
+*/
